9b2eb2ee29ab0e68a475f0e616d8f60c9b1f0a46
marthed <martinhedlund@live.com>
2018-04-01 10:17:22 +0200

    Fetch user data. Load more matches. Display more match data.

--- client/components/mainContainer.jsx
+++ client/components/mainContainer.jsx
@@ -7,14 +7,15 @@ export default class MainContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      matches: []
+      matches: [],
+      isFetching: false
     }
   }
   
   componentDidMount() {
     const matches = sessionStorage.getItem('matches');
     if (matches) {
-      console.log('set matches from session');
+      console.log('Set matches from session');
       this.setState({ matches: JSON.parse(matches) });
     }
   }
@@ -22,36 +23,66 @@ export default class MainContainer extends React.Component {
   getMatches = async () => {
     console.log('Getting matches...');
     const { accessToken } = this.props;
+    const pageToken = sessionStorage.getItem('pageToken');
+    this.setState({isFetching: true});
+
     try {
       const res = await axios.request({
         url: '/matches',
         method: 'POST',
         data: {
-          accessToken
+          accessToken,
+          pageToken
         }
       });
-      const { matches } = res.data.data;
-      sessionStorage.setItem('matches', JSON.stringify(matches));
-      this.setState({ matches });
+      const { matches, next_page_token} = res.data;
+      const storedMatches = sessionStorage.getItem('matches');
+      const allMatches = storedMatches ? JSON.parse(storedMatches).concat(matches) : matches;
+
+      sessionStorage.setItem('matches', JSON.stringify(allMatches));
+      sessionStorage.setItem('pageToken', JSON.stringify(next_page_token));
+      this.setState({ matches: allMatches });
+      this.setState({isFetching: false});
     }
     catch (error) {
+      this.setState({isFetching: false});
       console.log(error);
     }
   };
 
   renderMatches = () => {
-    const { matches } = this.state;
+    const { matches, isFetching } = this.state;
+    console.log('matches: ', matches);
+
+    if (!matches || matches.length === 0) return null;
     return matches.map((match, idx) => {
       return <MatchBox key={idx} match={match} />
-    });
+    })
+    .concat(
+      <div key="load-more" className="button-container__more-matches">
+        <button
+          disabled={isFetching}
+          onClick={this.getMatches}>
+          {isFetching ? "Hämtar..." :"Ladda fler matchningar"}
+          </button>
+      </div>
+    );
   }
 
+
   render () {
+    const { matches, isFetching } = this.state;
+
     return (
       <div className="main-container">
+        {matches.length === 0 && 
         <div className="button-container">
-          <button onClick={this.getMatches}>Get Matches</button>
-        </div>
+          <button
+            disabled={isFetching}
+            onClick={this.getMatches}>
+            {isFetching ? 'Hämtar...' : "Hämta matchningar"}
+          </button>
+        </div>}
         <div className="main-container__matches">
           {this.renderMatches()}
         </div>
--- client/components/matchBox.jsx
+++ client/components/matchBox.jsx
@@ -8,6 +8,18 @@ export default class MatchBox extends React.Component {
     return birthDate.split('T')[0];
   }
 
+  renderJobs = (jobs) => {
+    if (!jobs || jobs.length === 0) return null;
+    return jobs.map((job) => {
+      const { title, company } = job;
+      return (
+      <span key={company ? company.name : title.name}>
+        {title ? title.name : null}
+        {company ? company.name : null}
+      </span>)
+    });
+  }
+
   render() {
     const { match } = this.props;
 
@@ -15,9 +27,12 @@ export default class MatchBox extends React.Component {
       <div className="match-box">
         <span>
           <img src={match.person.photos[0].url} height="150"/>
-          <div>{match.person.name}</div>
+          <div><b>{match.person.name}</b></div>
           <div>{this.renderBirthDate(match.person.birth_date)}</div>
+          <div>Avstånd: {match.distance_mi} Km</div>
+          <div>{this.renderJobs(match.jobs)}</div>
         </span>
+        <br />
         <span>{match.person.bio}</span>
       </div>
       )
