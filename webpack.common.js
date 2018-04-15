722fd0d3521a89fd4096fa84f89685e0cbd85fb8
marthed <martinhedlund@live.com>
2018-04-15 14:53:15 +0200

    webpack split on dev / prod

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let loaders = [];
+
+loaders.push({
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: 'babel-loader',
+});
+
+loaders.push({
+  test: /\.css$/,
+  loaders: ['style-loader', 'css-loader']
+});
+
+module.exports = {
+  entry: {
+    app: './client/app.jsx'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ],
+  module: {
+    rules: loaders
+  },
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  }
+};
