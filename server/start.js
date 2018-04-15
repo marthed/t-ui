da8c08f96c414d664926792595e53347660df092
marthed <martinhedlund@live.com>
2018-04-15 16:09:09 +0200

    only use certs on local

--- server/start.js
+++ server/start.js
@@ -6,9 +6,19 @@ var certificate = fs.readFileSync('./server/ssl/server.crt');
 
 app.set('port', process.env.PORT || 7777);
 
-const server = https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app).listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+const certs = app.get('port') === 7777 ? 
+  {
+    key: privateKey,
+    cert: certificate
+  } : null;
+if (certs) {
+  const server = https.createServer(certs, app).listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+  });
+}
+else {
+  app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${app.get('port')}`);
+  });
+}
+
