0f5392245646574a2a2849146f29d193a3ff88eb
marthed <martinhedlund@live.com>
2018-04-15 21:55:25 +0200

    removed ssl certs from server

--- server/start.js
+++ server/start.js
@@ -1,24 +1,9 @@
 const app = require('./app');
 var fs = require('fs');
 var https = require('https');
-var privateKey = fs.readFileSync('./server/ssl/server.key');
-var certificate = fs.readFileSync('./server/ssl/server.crt');
 
 app.set('port', process.env.PORT || 7777);
-
-const certs = app.get('port') === 7777 ? 
-  {
-    key: privateKey,
-    cert: certificate
-  } : null;
-if (certs) {
-  const server = https.createServer(certs, app).listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
-}
-else {
-  app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${app.get('port')}`);
-  });
-}
+app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${app.get('port')}`);
+});
 
