ad6459c0a96bac838067a84352a275e9bab017b7
marthed <martinhedlund@live.com>
2018-04-17 22:29:15 +0200

    press button

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -10,6 +10,8 @@ const CONFIRM_BUTTOM_SELECTOR = '#platformDialogForm > div._5lnf.uiOverlayFooter
 const CONFIRM_URL = 'https://www.facebook.com/v2.6/dialog/oauth/confirm?dpr=';
 const FACEBOOK_LOGIN_URL = "https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd";
 
+const CONTINUE_BUTTON = '#checkpointSubmitButton';
+
 let browserWSEndPoint = null;
 async function getBrowser() {
   if (!browserWSEndPoint) {
@@ -79,9 +81,16 @@ module.exports = async function login(req, res) {
     await navResponse;
     await timeOut();
 
-    const theDoc = await page.content();
-    fs.writeFileSync('./public/first.html', theDoc);
-    console.log('COMPLETE');
+    const docOne = await page.content();
+    fs.writeFileSync('./public/first.html', docOne);
+
+    const button1 = await page.$(CONTINUE_BUTTON);
+    console.log('button: ', button1);
+
+    page.evaluate(e => e.click(), button1);
+    await timeOut();
+    await page.close();
+
     page.on('response', async response => {
       if (response.url().startsWith(CONFIRM_URL)) {
         const body = await response.text();
