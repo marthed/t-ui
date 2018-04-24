d4dd07566ac0f512440b0bdcc6f51c3145c3cf14
marthed <martinhedlund@live.com>
2018-04-24 22:05:11 +0200

    Third level

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -11,6 +11,7 @@ const CONFIRM_URL = 'https://www.facebook.com/v2.6/dialog/oauth/confirm?dpr=';
 const FACEBOOK_LOGIN_URL = "https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd";
 
 const CONTINUE_BUTTON = '#checkpointSubmitButton';
+const CONFIRM_RADIO_BUTTON ='#u_2_0';
 
 var BrowserEP = (function () {
   var instance;
@@ -102,20 +103,23 @@ module.exports = async function login(req, res) {
 
     await page.screenshot({path: './public/images/hej1.png'});
 
-    console.log('HEEJ');
-    //page.evaluate(e => e.click(), button1);
-    //const navResponse2 = page.waitForNavigation(['networkidle0']);
     page.click(CONTINUE_BUTTON);
     await timeOut();
     console.log('HEEJ2');
-
-
     await page.screenshot({path: './public/images/hej3.png'});
-
 
     const docTwo = await page.content();
     fs.writeFileSync('./public/second.html', docTwo);
 
+    page.click(CONFIRM_RADIO_BUTTON);
+    await timeOut();
+    console.log('Heej3');
+    await page.screenshot({path: './public/images/hej4.png'});
+
+    const docThree = await page.content();
+    fs.writeFileSync('./public/third.html', docTwo);
+
+    console.log('Hej4!');
 
     page.on('response', async response => {
       if (response.url().startsWith(CONFIRM_URL)) {
